ccb4783341071efb8ff27861d2d4f8b76f8f4899
Yash Chauhan <armanchauhan1@gmail.com>
2022-05-11 08:24:13 +0530

    Adding code to app.py

---
+++ MyEnsembledClassifier.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+class EnsembledModel:
+
+  #Initiation
+  def __init__(self, models, weights):
+    self.__models = models
+    self.__weights = weights
+
+  #Function to Calculate sepreat prediction probabilities
+  def __weighted_probs(self, X):
+    weighted_probs = []
+    for model,weight in zip(self.__models, self.__weights):
+      weighted_probs.append(model.predict_proba(X) * weight)
+    return weighted_probs
+  
+  #Funstion to calculate ensembler prediction probability
+  def predict_proba(self, X):
+    predict_probs = sum(self.__weighted_probs(X))
+    return np.array(predict_probs)
+
+  #Function to calculate prediction of the ensembler
+  def predict(self, X):
+    preds = []
+    predict_probs = sum(self.__weighted_probs(X))
+    for probs in predict_probs:
+      preds.append(np.argmax(probs))
+    return np.array(preds)
--- app.py
+++ app.py
@@ -1,3 +1,47 @@
 #Importing dependencies
 from flask import Flask
 from flask import render_template, request
+import numpy as np
+import joblib
+import xgboost as xgb
+import catboost as cb
+import os
+
+#Loading Models
+#XGB
+xgbc = xgb.XGBClassifier()
+path = os.path.join('saved_models', 'XGBClassifier.json')
+xgbc.load_model(path)
+
+#CBC
+cbc = cb.CatBoostClassifier()
+path = os.path.join('saved_models', 'CBClassifier.json')
+cbc.load_model(path)
+
+#Random Forest
+path = os.path.join('saved_models', 'RFClassifier.sav')
+with open(path, 'rb') as f:
+  rfc = joblib.load(f)
+
+#Ensembled Class
+from MyEnsembledClassifier import EnsembledModel
+models = [cbc, xgbc, rfc]
+weights = [0.35, 0.35, 0.30]
+emc = EnsembledModel(models, weights)
+
+#Application Framework
+app = Flask(__name__)
+
+@app.route("/", methods=['GET'])
+def index_page():
+    return  render_template("index.html")
+
+@app.route('/predict', methods=['POST'])
+def prediction():
+    model = request.form['model'].split()[-1][0]
+
+    return render_template("Test.html", model=model)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
