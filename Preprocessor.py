1c1a0f1310d9a4b1d7d94b833b55f6c6ecd4870c
Yash Chauhan <armanchauhan1@gmail.com>
2022-05-11 17:55:26 +0530

    Final Commit

--- Preprocessor.py
+++ Preprocessor.py
@@ -72,16 +72,32 @@ def extract_features(audio_path):
 
     #Creating datafram
     df = pd.DataFrame(feature_dic)
+    cols = ['Tempo', 'Chroma_STFT_Mean', 'Chroma_STFT_Var', 'RMS_Mean', 'RMS_Var',
+       'Spectral_Centroid_Mean', 'Spectral_Centroid_Var',
+       'Spectral_Bandwidth_Mean', 'Spectral_Bandwidth_Var',
+       'Spectral_Rolloff_Mean', 'Spectral_Rolloff_Var',
+       'Zero_Crossing_Rate_Mean', 'Zero_Crossing_Rate_Var', 'Harmony_Mean',
+       'Harmony_Var', 'Perceptr_Mean', 'Perceptr_Var', 'MFCC1_Mean',
+       'MFCC1_Var', 'MFCC2_Mean', 'MFCC2_Var', 'MFCC3_Mean', 'MFCC3_Var',
+       'MFCC4_Mean', 'MFCC4_Var', 'MFCC5_Mean', 'MFCC5_Var', 'MFCC6_Mean',
+       'MFCC6_Var', 'MFCC7_Mean', 'MFCC7_Var', 'MFCC8_Mean', 'MFCC8_Var',
+       'MFCC9_Mean', 'MFCC9_Var', 'MFCC10_Mean', 'MFCC10_Var', 'MFCC11_Mean',
+       'MFCC11_Var', 'MFCC12_Mean', 'MFCC12_Var', 'MFCC13_Mean', 'MFCC13_Var',
+       'MFCC14_Mean', 'MFCC14_Var', 'MFCC15_Mean', 'MFCC15_Var', 'MFCC16_Mean',
+       'MFCC16_Var', 'MFCC17_Mean', 'MFCC17_Var', 'MFCC18_Mean', 'MFCC18_Var',
+       'MFCC19_Mean', 'MFCC19_Var', 'MFCC20_Mean', 'MFCC20_Var']
+
+    df = df[cols]
 
     #Scaling
     with open('scaler.save', 'rb') as f:
         scaler = joblib.load(f)
 
-    cols = df.columns
     df = pd.DataFrame(scaler.transform(df), columns = cols)
-
-    return df
+    return df.values[0]
 
 if __name__ == '__main__':
 
-    print(extract_features(os.path.join('static','blues.00007.wav')).values)
+    path = os.path.join('static','blues.00007.wav')
+    print(extract_features(path))
+    ch = input('')
--- app.py
+++ app.py
@@ -6,32 +6,73 @@ import joblib
 import xgboost as xgb
 import catboost as cb
 import os
+import Preprocessor as prep
+import matplotlib.pyplot as plt
+import librosa
+import librosa.display
+
+#Labels
+label = {0: 'Blues', 1: 'Classical', 2: 'Country', 3: 'Disco',
+        4: 'Hiphop', 5: 'Jazz', 6: 'Metal', 7: 'Pop', 8: 'Reggae', 9: 'Rock'}
 
 #Loading Models
 #XGB
-xgbc = xgb.XGBClassifier()
-path = os.path.join('saved_models', 'XGBClassifier.json')
-xgbc.load_model(path)
-
+import saved_models.XGBCNative as xgbc
 #CBC
 cbc = cb.CatBoostClassifier()
 path = os.path.join('saved_models', 'CBClassifier.json')
 cbc.load_model(path)
-
 #Random Forest
-path = os.path.join('saved_models', 'RFClassifier.sav')
-with open(path, 'rb') as f:
-  rfc = joblib.load(f)
-
+import saved_models.RFCNative as rfc
 #Ensembled Class
 from MyEnsembledClassifier import EnsembledModel
 models = [cbc, xgbc, rfc]
 weights = [0.35, 0.35, 0.30]
 emc = EnsembledModel(models, weights)
 
-#Labels
-label = {0: 'blues', 1: 'classical', 2: 'country', 3: 'disco',
-        4: 'hiphop', 5: 'jazz', 6: 'metal', 7: 'pop', 8: 'reggae', 9: 'rock'}
+#Function for prediction
+def predict(audio_path,model):
+
+    #Extracting Features
+    features = prep.extract_features(audio_path)
+
+    #Predicting
+    if model == 'X':
+        model_name = 'XGBoost'
+        probs = xgbc.predict_proba(features)
+
+    elif model == 'C':
+        model_name = 'CatBoost'
+        probs = cbc.predict_proba(features)
+
+    elif model == 'F':
+        model_name = 'Random Forest'
+        probs = rfc.predict_proba(features)
+
+    else:
+        model_name = 'Ensembled Model'
+        probs = emc.predict_proba(features)
+
+    #Decoding Label
+    pred_prob = '{:.2f}'.format(probs[np.argmax(probs)])
+    pred = label[np.argmax(probs)]
+
+    return pred, pred_prob, model_name
+
+#Function to visualize MFCC
+def get_mfcc(audio_path):
+
+    audio_lb, sr = librosa.load(audio_path)
+    chromagram = librosa.feature.chroma_stft(audio_lb, sr=sr)
+    print('Chromogram shape:', chromagram.shape, '\n')
+
+    plt.figure(figsize=(16, 6))
+    librosa.display.specshow(chromagram, x_axis='time', y_axis='chroma', cmap='coolwarm');
+    #saving
+    img_path = os.path.join('static', 'Chroma_Frequencies.png')
+    plt.savefig(img_path, bbox_inches='tight', dpi=200)
+
+    return img_path
 
 #Application Framework
 app = Flask(__name__)
@@ -48,14 +89,18 @@ def prediction():
     audio_path = 'static/' + audio.filename
     audio.save(audio_path)
 
-    #Extracting Features
-    import Preprocessor.py
-    features = extract_features(audio_path)
+    #getting mfcc visualization
+    img_path = get_mfcc(audio_path)
 
+    #Checking User Model Selection
     model = request.form['model'].split()[-1][0]
 
-    return render_template("Test.html", model=model)
+    #Getting Prediction
+    pred, pred_prob, model_name = predict(audio_path, model)
+
+    return render_template("prediction.html", pred=pred, pred_prob=pred_prob,
+                            model_name=model_name, img_path=img_path)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
